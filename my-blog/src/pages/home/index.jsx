6e5cd6ec1411af5b7a4490b13040b82c31645dc6
GustavoKozlowski <gustavokozlowski96@gmail.com>
2022-08-22 21:01:18 -0300

    testes com posts mockados

--- my-blog/src/pages/home/index.jsx
+++ my-blog/src/pages/home/index.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 import FeaturePost from "../../components/featurespost";
-
-
 import PostList from "../../components/postlist";
 export default function Home() {
   const posts = [
     {
-        image: 'https://coisasdojapao.com/wp-content/uploads/2016/12/monte-fuji.jpg',
-        date :"January 28, 2021",
-        title:"Monte Fuji",
-        description :"Apenas uma bela foto do explendoso Monte Fuji.",
-        alt: 'Monte fuji',
+      image:
+        "https://coisasdojapao.com/wp-content/uploads/2016/12/monte-fuji.jpg",
+      date: "January 28, 2021",
+      title: "Monte Fuji",
+      description: "Apenas uma bela foto do explendoso Monte Fuji.",
+      alt: "Monte fuji",
     },
-  ]
+    {
+      image:
+        "https://magazine.zarpo.com.br/wp-content/uploads/2022/02/capa_praia-do-caribe_zarpo.jpg",
+      date: "January 28, 2021",
+      title: "Monte Fuji",
+      description: "Apenas uma bela foto do explendoso Monte Fuji.",
+      alt: "Monte fuji",
+    },
+    {
+      image:
+        "https://p1.pxfuel.com/preview/777/334/535/cactus-sunrise-desert-landscape.jpg",
+      date: "January 28, 2021",
+      title: "Monte Fuji",
+      description: "Apenas uma bela foto do explendoso Monte Fuji.",
+      alt: "Monte fuji",
+    },
+  ];
   return (
     <div>
-      <FeaturePost />
+     {
+        posts  ? <FeaturePost post={posts[0]}  /> : "nada"
+     }
 
       <PostList posts={posts} />
     </div>
